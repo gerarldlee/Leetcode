41366003a19d1fe0fdd42c93c1f281ddab8f8dcd
Gerard <gerarldlee@gmail.com>
2022-06-11 11:49:27 -0700

    tp

--- src/com/algomonster/tp/MinimumWindowSubstring.java
+++ src/com/algomonster/tp/MinimumWindowSubstring.java
@@ -3,64 +3,61 @@ package com.algomonster.tp;
 import java.util.*;
 
 public class MinimumWindowSubstring {
-    public static boolean isWindowComplete(HashMap<Character, Integer> checkmap, HashMap<Character, Integer> window) {
-        // the size of running window should at least be greater than checkmap
-        if (checkmap.size() > window.size()) return false;
-
-        // check the window against the checkmap, and deduct the counts
-        // all the chars from checkmap should be in the window, but not vice versa
-        for (Character c : checkmap.keySet()) {
-            if (!window.containsKey(c))
-                return false;
-            if (window.get(c) - checkmap.get(c) < 0)
-                return false;
-        }
-        return true;
-    }
 
     public static String getMinimumWindow(String original, String check) {
         // WRITE YOUR BRILLIANT CODE HERE
 
-        if (original.length() < check.length()) return "";
+        if (original == null || check == null) return "";
 
-        // checkmap counts
-        HashMap<Character, Integer> checkmap = new HashMap<>();
-        for (Character c: check.toCharArray()) {
-            checkmap.put(c, checkmap.getOrDefault(c, 0) + 1);
-        }
-        // running window map
-        HashMap<Character, Integer> runningwindow = new HashMap<>();
-        int absolute_l = 0, absolute_r = 0, absolute_length = original.length();
-        String window = original;
+        // put all the check into a count map
+        HashMap<Character, Integer> countMap = new HashMap<>();
+        for (char c : check.toCharArray()) countMap.put(c, countMap.getOrDefault(c, 0)+1);
 
-        for (int left=0, right=0; right < original.length(); right++) {
+        // begin moving right pointer, and within the window, put everything to the window map
+        // once everything is in the window map, try to decrease the window size and the map
+        // but record the left and right window first.
+        // try to find a smaller window by moving the right to the right, until all of the original
+        // length is finished. then return the window
+        int left=0, right=0;
+        int start=left, windowSize=Integer.MAX_VALUE;
+        HashMap<Character, Integer> windowMap = new HashMap<>();
+        int trackWindowCount=0;    // we also need a tracker for the correct elements in the window map
+        for (; right < original.length(); right++) {
             char c = original.charAt(right);
-            runningwindow.put(c, runningwindow.getOrDefault(c, 0) + 1);
+            windowMap.put(c, windowMap.getOrDefault(c,0)+1);
 
-            // try to compare if the window is valid and complete
-            if (isWindowComplete(checkmap, runningwindow)) {
+            if (countMap.containsKey(c) && windowMap.containsKey(c) && countMap.get(c) == windowMap.get(c)) {
+                trackWindowCount++;
+            }
 
-                // try to shrink the window then
-                while(left <= right && isWindowComplete(checkmap, runningwindow)) {
-                    // is this lexicographically smaller than the previous?
-                    String current = original.substring(left, right+1);
-                    if (right+1 - left <= absolute_length && current.compareTo(window) < 0) {
-                        absolute_l = left;
-                        absolute_r = right+1;
-                        absolute_length = absolute_r - absolute_l;
-                        window = current;
-                    }
-
-                    // remove the left value from the runningwindow
-                    char l = original.charAt(left);
-                    runningwindow.put(l, runningwindow.get(l) - 1);
-
-                    left++;
+            while (trackWindowCount == countMap.size()) {
+                // update the result. we need to compare if the window is shorter than the previous window
+                if (windowSize == Integer.MAX_VALUE || right+1 - left < windowSize) {
+                    start = left;
+                    windowSize = (right+1 - left);
                 }
+                else if (right+1 - left == windowSize) {
+                    // if they are the same length though, compare its lexicographically smaller
+                    String previous = original.substring(start, windowSize+start);
+                    String current = original.substring(left, right+1);
+                    if (previous.compareTo(current) > 0) {
+                        start = left;
+                        windowSize = (right+1 - left);
+                    }
+                }
+
+                // decrease the window
+                char l = original.charAt(left);
+                windowMap.put(l, windowMap.get(l)-1);
+                if (countMap.containsKey(l) && windowMap.get(l) < countMap.get(l)) {
+                    trackWindowCount--;
+                }
+                left++;
             }
         }
 
-        return original.substring(absolute_l, absolute_r);
+        if (windowSize == Integer.MAX_VALUE) return "";
+        return original.substring(start, windowSize+start);
     }
 
     public static void main(String[] args) {
