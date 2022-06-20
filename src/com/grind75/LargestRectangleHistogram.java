31c78d98be66a8f291344e67d3dd14615a0176f4
Gerard <gerarldlee@gmail.com>
2022-06-20 13:05:58 -0700

    unfinished

---
+++ src/com/grind75/LargestRectangleHistogram.java
@@ -0,0 +1,10 @@
+package com.grind75;
+
+public class LargestRectangleHistogram {
+    public int largestRectangleArea(int[] heights) {
+        if (heights == null || heights.length == 0) return 0;
+//        return area(heights, 0, heights.length-1);
+        return 0;
+    }
+
+}
